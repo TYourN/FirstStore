8cdb0a97d24358dca0de9a90eb5020fa3fbfee7d
TYourN <17855833945m@sina.cn>
2017-02-22 10:03:56 +0800

    twenty

---
+++ springmvc_mybatis/src/com/xian/ssm/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.xian.ssm.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	//µÇÂ¼
+	@RequestMapping("/login")
+	public String login(HttpSession session,String username,String password) throws Exception{
+		session.setAttribute("username", username);
+		return "redirect:/items/queryItems.action";
+	}
+	
+	
+	//ÍË³ö
+	@RequestMapping("/logout")
+	public String logout(HttpSession session)throws Exception{
+		session.invalidate();
+		return "redirect:/items/queryItems.action";
+	}
+}
